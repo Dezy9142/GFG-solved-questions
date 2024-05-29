cb83286369904639ccdea4e5891296d172084d76
Dezy <89174521+Dezy9142@users.noreply.github.com>
2024-05-29 19:33:32 +0530

    Time: 92 ms, Memory: 73 MB - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -3,15 +3,14 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int>mp;
         for(auto &num:nums){
+            if(mp.find(num)!=mp.end()){
+                return true;
+            }
             mp[num]++;
         }
-        for(auto &[x,f]:mp){
-            if(f>1){
-                return true;
-                break;
-            }
-        }
         return false;
+        
+        
         
     }
 };
